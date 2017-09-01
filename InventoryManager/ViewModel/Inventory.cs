efbdbf8881123236c8d6aef784fc82dec0f0508f
Bishan <bishan.da@live.com>
2017-08-31 20:13:38 -0700

    Add ViewModels for HardwareController

---
+++ InventoryManager/ViewModel/Inventory.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InventoryManager.Models;
+
+namespace InventoryManager.ViewModel
+{
+    public class HardwareData : ViewModel
+    {
+        public Hardware Hardware { get; set; }
+        public IEnumerable<HardwareType> HardwareTypes { get; set; }
+        public IEnumerable<Maker> Makers { get; set; }
+        public IEnumerable<Vendor> Vendors { get; set; }
+
+    }
+
+    public class HardwareList : ViewModel
+    {
+        public IList<Hardware> Hardwares;
+    }
+}
