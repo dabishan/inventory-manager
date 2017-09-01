2aadbe102e94442e3bfed1ffe32ef058740d6ba2
Bishan <bishan.da@live.com>
2017-09-01 09:35:47 -0700

    Added Controller for Owner

---
+++ InventoryManager/Controllers/OwnerController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManager.Models;
+
+namespace InventoryManager.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private InventoryContext db = new InventoryContext();
+
+        // GET: Owner
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
