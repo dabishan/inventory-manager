84e1fdd1c9e5b81ac1c16b63c4a5468b932f9e0a
Bishan <bishan.da@live.com>
2017-09-11 19:57:57 -0700

    Added User Controller and ViewModel

---
+++ InventoryManager/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManager.ViewModel;
+
+namespace InventoryManager.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View( new LoginData());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public ActionResult Login(LoginData l)
+        {
+            if(!ModelState.IsValid) return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+
+            return View(l);
+        }
+
+        public ActionResult Logout()
+        {
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ InventoryManager/ViewModel/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InventoryManager.ViewModel
+{
+    public class LoginData : ViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
