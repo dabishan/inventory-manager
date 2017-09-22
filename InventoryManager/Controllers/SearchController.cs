35f144ff4d2381a1d9ec7794dfd753733f6cd5c3
Bishan <bishan.da@live.com>
2017-09-21 18:21:02 -0700

    Added Controller and View and Modified Header of Seach

---
+++ InventoryManager/Controllers/SearchController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using InventoryManager.Models;
+using InventoryManager.ViewModel;
+
+namespace InventoryManager.Controllers
+{
+    public class SearchController : Controller
+    {
+        private InventoryContext db = new InventoryContext();
+
+        // GET: Search
+        public ActionResult Index(string query)
+        {
+            var viewModel = new SearchAllView()
+            {
+                Query = query,
+                Hardwares = new List<Hardware>(),
+                Customers = new List<Customer>(),
+                Contacts = new List<Contact>()
+            };
+
+            if (string.IsNullOrWhiteSpace(query) || query.Length < 3)
+            {
+                ViewBag.InvalidQuery = true;
+                return View(viewModel);
+            }
+            else
+            {
+                ViewBag.InvalidQuery = false;
+            }
+
+            query = query.ToLower();
+
+            viewModel.Hardwares = SearchHardware(query).Include(i=> i.Inventory).ToList();
+            viewModel.Customers = SearchCustomers(query).Include(c => c.Owner).ToList();
+            viewModel.Contacts = SearchContacts(query).Include(c => c.Customer.Owner).ToList();
+
+            return View(viewModel);
+        }
+
+        public IQueryable<Hardware> SearchHardware(string query)
+        {
+            var hardwares = db.Hardwares
+                .Where(i => i.Inventory.Name.ToLower().Contains(query)
+                            || i.Inventory.ModelName.ToLower().Contains(query));
+
+            return hardwares;
+        }
+
+        public IQueryable<Customer> SearchCustomers(string query)
+        {
+            var customers = db.Customers
+                .Where(c => c.Owner.Name.ToLower().Contains(query));
+
+            return customers;
+        }
+
+        public IQueryable<Contact> SearchContacts(string query)
+        {
+            var contacts = db.Contacts
+                .Where(c => c.FirstName.ToLower().Contains(query)
+                    || c.LastName.ToLower().Contains(query));
+                    
+            return contacts;
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ InventoryManager/ViewModel/Search.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using InventoryManager.Models;
+
+namespace InventoryManager.ViewModel
+{
+    public class SearchAllView
+    {
+        public string Query { get; set; }
+        public ICollection<Hardware> Hardwares { get; set; }
+        public ICollection<Customer> Customers { get; set; }
+        public ICollection<Contact> Contacts { get; set; }
+    }
+}
